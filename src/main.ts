bc335d97731978264f7841fed6b7be63c1d43093
Miracle Boniface <udohmiracle23@gmail.com>
2025-05-12 21:43:46 +0100

    repo updated

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
-
-let cachedApp:any = null;
 
 async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule);
-    await app.init();
-    cachedApp = app.getHttpAdapter().getInstance();
-  }
-  return cachedApp;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-export default async function handler(req, res) {
-  const app = await bootstrap();
-  return app(req, res);
-}
+bootstrap();
