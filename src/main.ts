a2b7cd9186dfc3ffb57d84ccd42fd7054b839ba4
Miracle Boniface <udohmiracle23@gmail.com>
2025-06-10 03:47:40 +0100

    repo updated

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,14 @@ console.log(port)
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); app.useGlobalPipes(
+  const app = await NestFactory.create(AppModule); 
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://rejah.net', 'https://www.rejah.net'], // allow requests from your frontend
+    credentials: true,               // allow cookies/auth headers if needed
+  });
+
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // strips properties not in the DTO
       forbidNonWhitelisted: true, // throws error if extra properties
